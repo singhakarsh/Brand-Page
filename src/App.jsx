4c0b7aa5599a5d38e94fa348f7eb9d799e8b824b
Akarsh Singh <singhakarsh18@gmail.com>
2024-08-11 17:31:21 +0530

    project-1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HeroSection from "./components/Hero";
+import Navigation from "./Components/Navigation";
+
+const App = () => {
+    return (
+    <div>
+        <Navigation />
+        <HeroSection />
+    </div>
+    );
+};
+export default App;
